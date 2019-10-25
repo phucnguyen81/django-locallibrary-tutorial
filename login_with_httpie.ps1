96a06910d4ecc7a07243ae1f01bd51254f55d24a
phucknguyen <phucknguyen@gmail.com>
2019-10-25 10:29:04 +0700

    Update powershell script for httpie

--- login_with_httpie.ps1
+++ login_with_httpie.ps1
@@ -1,16 +1,21 @@
+$username = 'phuc'
+$password = 'phuc'
+$session = 'session3'
+
 # Get csrftoken
-http localhost:8000/accounts/login/ --session=session2 -h
+http localhost:8000/accounts/login/ --session=$session -h
 # Response looks like:
 # HTTP/1.1 200 OK
 # Set-Cookie: csrftoken=euogGDvtNo8JH6f5hWHm3yG7H0Lq9bbF72zLqAwSxphKKQgh2k77tyaWMBfKgUtF
 
 # TODO parse response (e.g. with jq) to get the csrf token
+$csrf_token = '2C5z21Ex4Wv09ZUkeujISsy38BNHlOjSEpVsdEAU1yyQMavtMfqmtSMJ3JGqyHSN'
 
 # Login with the response CRRFToken
 http -f POST localhost:8000/accounts/login/ `
-    username=phuc password=phuc `
-    X-CSRFToken:euogGDvtNo8JH6f5hWHm3yG7H0Lq9bbF72zLqAwSxphKKQgh2k77tyaWMBfKgUtF `
-    --session=session2 -h
+    username=$username password=$password `
+    X-CSRFToken:$csrf_token `
+    --session=$session --follow -h
 # Response looks like:
 # HTTP/1.1 302 Found
 # Location: /
@@ -18,6 +23,6 @@ http -f POST localhost:8000/accounts/login/ `
 # Set-Cookie: sessionid=p0cochytq080zmbk6dncw0jiyukcvbw9
 
 # Request secure data
-http localhost:8000/catalog/mybooks/ --session=session2 -h
+http localhost:8000/catalog/mybooks/ --session=$session -h
 # Response looks like:
 # HTTP/1.1 200 OK
